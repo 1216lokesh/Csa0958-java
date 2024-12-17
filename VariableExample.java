729ea0930635559209bdd38afcb27d85693ac9f5
1216lokesh <lokeshlokeshlokey2@gmail.com>
2024-12-17 08:58:13 +0530

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,29 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        // Boolean variable
+        boolean result = true;
+
+        // Character variable
+        char capitalC = 'C';
+
+        // Byte variable
+        byte b = 100;
+
+        // Short variable
+        short s = 10000;
+
+        // Integer variable
+        int i = 100000;
+
+        // Long variable with underscore for readability and 'L' suffix
+        long creditCardNumber = 1234_5678_9012_3456L;
+
+        // Printing all variables
+        System.out.println("Boolean result: " + result);
+        System.out.println("Character capitalC: " + capitalC);
+        System.out.println("Byte b: " + b);
+        System.out.println("Short s: " + s);
+        System.out.println("Integer i: " + i);
+        System.out.println("Long creditCardNumber: " + creditCardNumber);
+    }
+}
